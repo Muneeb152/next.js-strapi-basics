1189093d429d913701b82980e930453578e4372d
muneeb_akhtar <muneeb@superfuture.world>
2023-06-06 17:59:30 +0400

    Latest code

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function PostPreview({post}) {
   return (
    <>
+ <Link href={`/posts/${post.id}`}>
    <div className='postPreview'>
    <h3>{post.title}</h3>
    <p>{post.description}</p>
    </div>
+   </Link>
    </>
   )
 }
